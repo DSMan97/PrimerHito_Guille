f722bac714901904a166bba03efb7dc25aa22f30
GUILLERMO CRESPO AGUAYO <guillermo.crespo@utmac0014.local>
2018-04-05 12:17:07 +0200

    Prueba

---
+++ PrimerHito_Guille/TablasViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TablasViewController.swift
+//  PrimerHito_Guille
+//
+//  Created by GUILLERMO CRESPO AGUAYO on 5/4/18.
+//  Copyright © 2018 GUILLERMO CRESPO AGUAYO. All rights reserved.
+//
+
+import UIKit
+
+class TablasViewController: UIViewController {
+    @IBOutlet var lbTabla:TablasViewController?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ PrimerHito_Guille/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PrimerHito_Guille
+//
+//  Created by GUILLERMO CRESPO AGUAYO on 5/4/18.
+//  Copyright © 2018 GUILLERMO CRESPO AGUAYO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
